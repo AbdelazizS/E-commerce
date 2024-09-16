2826d5b8630a8a975247f3e1e97269f00f791d0f
John Doe1~ <abdelazizelrasheed@gmail.com>
2024-09-16 15:08:51 +0200

    solving conflict errors

--- src/views/OrderDetails.vue
+++ src/views/OrderDetails.vue
@@ -42,46 +42,47 @@ onMounted(() => {
     <div class="py-8 md:mb-16 Orders_details">
       <div class="grid md:grid-cols-2 md:gap-4 gap-6">
         <div class="fade-up bg-card shadow-md border max-w-lg p-4 relative">
-    <div class="py-8 md:mb-16">
-      <div class="grid md:grid-cols-2 md:gap-4 gap-6">
-        <div class="bg-card shadow-md border max-w-lg p-4 relative">
-          </div>
-          <div class="flex items-center gap-4 md:gap-6 items-">
-            <div class="w-36 h-26 md:w-40 md:h-40">
-              <img
-                src="/src/assets/pc.png"
-                alt="product-image"
-                class="w-full rounded-lg h-full object-cover"
-              />
-            </div>
+          <div class="py-8 md:mb-16">
+            <div class="grid md:grid-cols-2 md:gap-4 gap-6">
+              <div class="bg-card shadow-md border max-w-lg p-4 relative"></div>
+              <div class="flex items-center gap-4 md:gap-6 items-">
+                <div class="w-36 h-26 md:w-40 md:h-40">
+                  <img
+                    src="/src/assets/pc.png"
+                    alt="product-image"
+                    class="w-full rounded-lg h-full object-cover"
+                  />
+                </div>
 
-            <div class="space-y-2">
-              <h2 class="text-lg font-bold text-foreground">Apple iMac 24</h2>
-              <div class="flex items-center text-muted-foreground">
-                <p class="text-base">259.000 $</p>
+                <div class="space-y-2">
+                  <h2 class="text-lg font-bold text-foreground">Apple iMac 24</h2>
+                  <div class="flex items-center text-muted-foreground">
+                    <p class="text-base">259.000 $</p>
+                  </div>
+                </div>
               </div>
             </div>
-          </div>
-        </div>
-        <div class="fade-up bg-card shadow-md border max-w-lg p-4 relative">
-          <div
-            class="bg-primary w-8 h-8 md:h-10 md:w-10 flex items-center justify-center rounded-full cursor-pointer absolute top-2 ltr:right-2 rtl:left-2 rtl:md:left-4 md:top-4 ltr:md:right-4 text-white font-bold"
-          >
-            2
-          </div>
-          <div class="flex items-center gap-4 md:gap-6 items-">
-            <div class="w-36 h-26 md:w-40 md:h-40">
-              <img
-                src="/src/assets/pc.png"
-                alt="product-image"
-                class="w-full rounded-lg h-full object-cover"
-              />
-            </div>
+            <div class="fade-up bg-card shadow-md border max-w-lg p-4 relative">
+              <div
+                class="bg-primary w-8 h-8 md:h-10 md:w-10 flex items-center justify-center rounded-full cursor-pointer absolute top-2 ltr:right-2 rtl:left-2 rtl:md:left-4 md:top-4 ltr:md:right-4 text-white font-bold"
+              >
+                2
+              </div>
+              <div class="flex items-center gap-4 md:gap-6 items-">
+                <div class="w-36 h-26 md:w-40 md:h-40">
+                  <img
+                    src="/src/assets/pc.png"
+                    alt="product-image"
+                    class="w-full rounded-lg h-full object-cover"
+                  />
+                </div>
 
-            <div class="space-y-2">
-              <h2 class="text-lg font-bold text-foreground">Apple iMac 24</h2>
-              <div class="flex items-center text-muted-foreground">
-                <p class="text-base">259.000 $</p>
+                <div class="space-y-2">
+                  <h2 class="text-lg font-bold text-foreground">Apple iMac 24</h2>
+                  <div class="flex items-center text-muted-foreground">
+                    <p class="text-base">259.000 $</p>
+                  </div>
+                </div>
               </div>
             </div>
           </div>
